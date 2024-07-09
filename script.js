4b2c746e92faa412400560f73d19882178223443
Zenox2003 <smith.lindley93313@gmail.com>
2024-07-09 17:26:13 -0400

    first exercise

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ console.log("Js Functions Lab");
 
 // Exercise 1 
 
+const maxOfTwoNumber = (x, y) => {
+    if (x >= y) {
+        return x;
+    } else {
+      return y;
+    }  
+}
+
+console.log(`Exercise 1 Result:`, maxOfTwoNumber(5, 8));
+
