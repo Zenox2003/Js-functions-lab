7df6ec4c636f300922aa9d508b85512ed40e95b1
Zenox2003 <smith.lindley93313@gmail.com>
2024-07-09 17:30:58 -0400

    second exercise

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ const maxOfTwoNumber = (x, y) => {
 
 console.log(`Exercise 1 Result:`, maxOfTwoNumber(5, 8));
 
+// Exercise 2
+
+const isAdult = (number) => {
+    if (number >= 18) {
+    return "Adult";
+    }else {
+    return "Minor";
+    }
+}
+
+console.log(`Exercise 2 Result:`, isAdult(19));
